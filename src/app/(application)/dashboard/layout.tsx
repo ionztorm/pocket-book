c7080bec7f04185b7cc6aadd2c126c3f7be00949
ionztorm <leon.codes@icloud.com>
2024-11-20 19:34:13 +0000

    create dashboard layout and types

--- src/app/(application)/dashboard/layout.tsx
+++ src/app/(application)/dashboard/layout.tsx
@@ -1,9 +1,6 @@
+import { SidebarProvider } from '@/components/ui/sidebar';
 import { DashboardLayoutProps } from '@/lib/types/dashboard/dashboard.types';
 
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
-	/* TODO:
-	 *  - build out sidebar in this file
-	 *  - create client sidebar component for access to pathname (dynamic menu content)
-	 */
-	return <>{children}</>;
+	return <SidebarProvider>{children}</SidebarProvider>;
 }
--- src/lib/types/dashboard/dashboard.types.ts
+++ src/lib/types/dashboard/dashboard.types.ts
@@ -1,3 +1,18 @@
-import { ChildrenProps } from '../global.types';
+import { ChildrenProps } from '@/lib/types/global.types';
 
 export type DashboardLayoutProps = ChildrenProps;
+export type AppPageSidebarProps = ChildrenProps;
+export type DynamicMenuData = Readonly<{
+	label: string;
+	action: string;
+	actionDescription: string;
+}>;
+export type DynamicSidebarMenuProps = Readonly<{
+	menuData: DynamicMenuData;
+	menuItems: DynamicMenuItems;
+}>;
+export type DynamicMenuItem = {
+	id: number;
+	label: string;
+};
+export type DynamicMenuItems = DynamicMenuItem[];
