7d478f88749c4f0b4d7b8e56ea2c97adf82c7f12
ionztorm <leon.codes@icloud.com>
2024-11-20 19:56:50 +0000

    setup finance page sidebar

--- src/app/(application)/dashboard/finance/page.tsx
+++ src/app/(application)/dashboard/finance/page.tsx
@@ -1,3 +1,17 @@
-export default function FinancePage() {
-	return <div>Finance</div>;
+import { AppPageSidebar } from '@/app/(application)/_components/app-page-sidebar';
+import { DynamicSidebarMenu } from '@/app/(application)/_components/dynamic-sidebar-menu';
+import { dynamicFinanceMenuData, tempFinanceMenuContent } from '@/lib/tempData';
+
+export default function TodoPage() {
+	/* TODO:
+	 * fetch todo dynamic menu items from db
+	 */
+	return (
+		<>
+			<AppPageSidebar>
+				<DynamicSidebarMenu menuData={dynamicFinanceMenuData} menuItems={tempFinanceMenuContent} />
+			</AppPageSidebar>
+			{/* main todo body content here*/}
+		</>
+	);
 }
--- src/lib/tempData.ts
+++ src/lib/tempData.ts
@@ -7,8 +7,19 @@ export const dynamicTodoMenuData: DynamicMenuData = {
 		'Projects allow you to group your todos by theme, making them simpler to manage and understand',
 };
 
+export const dynamicFinanceMenuData: DynamicMenuData = {
+	label: 'Accounts',
+	action: 'Add an account',
+	actionDescription: 'Add your accounts and cards and start tracking your debts and expenses',
+};
 export const tempTodoMenuContent: DynamicMenuItems = [
 	{ id: 1, label: 'Holiday' },
 	{ id: 2, label: 'Shopping' },
 	{ id: 3, label: 'Work' },
+];
+
+export const tempFinanceMenuContent: DynamicMenuItems = [
+	{ id: 1, label: 'Barclays' },
+	{ id: 2, label: 'Lloyds' },
+	{ id: 3, label: 'Very Store Account' },
 ];
