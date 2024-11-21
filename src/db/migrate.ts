80438b49a55c7a53f9c47faf7d7a7d063dc95d37
ionztorm <leon.codes@icloud.com>
2024-11-21 20:29:52 +0000

    drizzle migrate file

---
+++ src/db/migrate.ts
@@ -0,0 +1,16 @@
+import { migrate } from 'drizzle-orm/neon-http/migrator';
+import { db } from '@/db/index';
+
+const main = async () => {
+	try {
+		await migrate(db, {
+			migrationsFolder: 'src/db/migrations',
+		});
+		console.log('✅ Migration complete');
+	} catch (err) {
+		console.error('Error during migration: ', err);
+		process.exit(1);
+	}
+};
+
+main();
