503cdef88cf774ea9b86f5ebabbc11d5f36e1191
ionztorm <leon.codes@icloud.com>
2024-11-23 13:11:33 +0000

    enable auto snake case in drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,10 +6,11 @@ import { env } from '@/lib/validations/validators/env.server.validator';
 config();
 
 export default defineConfig({
-	schema: './src/db/schema.ts',
+	schema: './src/db/schema',
 	out: './src/db/migrations',
 	dialect: 'postgresql',
 	dbCredentials: {
 		url: env.DATABASE_URL,
 	},
+	casing: 'snake_case',
 });
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,17 +1,14 @@
 import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/neon-http';
-import { neon } from '@neondatabase/serverless';
 import { env } from '@/lib/validations/validators/env.server.validator';
 
 config({
 	path: '.env',
 });
 
-const sql = neon(env.DATABASE_URL);
-
 /*
  * NOTE:
  * export const db = drizzle(sql, { logger: true });
  */
 
-export const db = drizzle(sql);
+export const db = drizzle({ connection: env.DATABASE_URL, casing: 'snake_case' });
