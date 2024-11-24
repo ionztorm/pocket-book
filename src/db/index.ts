9bb7574e2dde33f774a1c0654d7d36d41d0b2aad
ionztorm <leon.codes@icloud.com>
2024-11-24 18:15:58 +0000

    remove dotenv config calls - causing error
    not compatible with edge run time. To figure out later

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,10 @@
-import { config } from 'dotenv';
+//import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/neon-http';
 import { env } from '@/lib/validations/validators/env.server.validator';
 
-config({
-	path: '.env',
-});
+//config({
+//	path: '.env',
+//});
 
 /*
  * NOTE:
--- src/lib/validations/validators/env.server.validator.ts
+++ src/lib/validations/validators/env.server.validator.ts
@@ -1,7 +1,3 @@
-import { config } from 'dotenv';
 import { envSchema } from '@/lib/validations/schema/env.var.schema';
-
-// NOTE: ensure .env is loaded
-config();
 
 export const env = envSchema.parse(process.env);
