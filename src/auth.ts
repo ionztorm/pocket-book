93c945c297f4a29f5cb9497aa7543ccab08488ac
ionztorm <leon.codes@icloud.com>
2024-11-24 18:14:45 +0000

    setup google provider and drizzle db adapter

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,15 @@
 import NextAuth from 'next-auth';
+import Google from 'next-auth/providers/google';
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
+import { db } from '@/db/index';
+import { accounts, sessions, users, verificationTokens } from './db/schema/authentication';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-	providers: [],
+	adapter: DrizzleAdapter(db, {
+		usersTable: users,
+		accountsTable: accounts,
+		sessionsTable: sessions,
+		verificationTokensTable: verificationTokens,
+	}),
+	providers: [Google],
 });
