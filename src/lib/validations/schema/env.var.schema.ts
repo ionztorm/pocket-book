c867a517d0dc4a02222dbf3e9f629fabfb18b3d6
ionztorm <leon.codes@icloud.com>
2024-11-24 19:28:46 +0000

    add new env vars to validation

--- src/lib/validations/schema/env.var.schema.ts
+++ src/lib/validations/schema/env.var.schema.ts
@@ -2,4 +2,7 @@ import { z } from 'zod';
 
 export const envSchema = z.object({
 	DATABASE_URL: z.string().min(1),
+	AUTH_SECRET: z.string().min(1),
+	AUTH_GOOGLE_ID: z.string().min(1),
+	AUTH_GOOGLE_SECRET: z.string().min(1),
 });
