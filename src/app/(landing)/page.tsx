b146589d7a09f1db14f3355eab07326e0245ddec
ionztorm <leon.codes@icloud.com>
2024-11-26 19:41:38 +0000

    basic landing page for button testing

---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,26 @@
+import { headers } from 'next/headers';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { UserMenu } from '@/components/user-menu';
+import { auth } from '@/lib/auth';
+
+export default async function Home() {
+	const session = await auth.api.getSession({
+		headers: await headers(),
+	});
+	return (
+		<div className='flex justify-between p-4'>
+			<span>Pocket Book Landing Page</span>
+			{!session && (
+				<Button asChild>
+					<Link href='/auth'>Login</Link>
+				</Button>
+			)}
+			{session && (
+				<div className='flex gap-3'>
+					<UserMenu type='landing' side='bottom' />
+				</div>
+			)}
+		</div>
+	);
+}
