8aad9bda41a3b28967313b76e932c3a83a553ee9
ionztorm <leon.codes@icloud.com>
2024-11-27 16:24:13 +0000

    update auth provider type: +github

--- src/lib/types/auth/auth.types.ts
+++ src/lib/types/auth/auth.types.ts
@@ -1,6 +1,6 @@
 import type { Dispatch, SetStateAction } from 'react';
 
-export type AuthLoginProviders = 'google';
+export type AuthLoginProviders = 'google' | 'github';
 export type AuthOptions = 'register' | 'login' | 'passwordReset';
 export type AuthPageComponentProps = Readonly<{
 	setState: Dispatch<SetStateAction<AuthOptions>>;
