51e54ab920662f44493733b4479a4590669d2e55
ionztorm <leon.codes@icloud.com>
2024-11-27 22:09:03 +0000

    update types in tempData

--- src/lib/tempData.ts
+++ src/lib/tempData.ts
@@ -1,24 +1,27 @@
-import type { DynamicMenuData, DynamicMenuItems } from '@/lib/types/dashboard/dashboard.types';
+import type {
+	SidebarAppSubMenuData,
+	SidebarAppSubMenuItems,
+} from '@/lib/types/dashboard/dashboard.types';
 
-export const dynamicTodoMenuData: DynamicMenuData = {
+export const dynamicTodoMenuData: SidebarAppSubMenuData = {
 	label: 'Projects',
 	action: 'Add a project',
 	actionDescription:
 		'Projects allow you to group your todos by theme, making them simpler to manage and understand',
 };
 
-export const dynamicFinanceMenuData: DynamicMenuData = {
+export const dynamicFinanceMenuData: SidebarAppSubMenuData = {
 	label: 'Accounts',
 	action: 'Add an account',
 	actionDescription: 'Add your accounts and cards and start tracking your debts and expenses',
 };
-export const tempTodoMenuContent: DynamicMenuItems = [
+export const tempTodoMenuContent: SidebarAppSubMenuItems = [
 	{ id: 1, label: 'Holiday' },
 	{ id: 2, label: 'Shopping' },
 	{ id: 3, label: 'Work' },
 ];
 
-export const tempFinanceMenuContent: DynamicMenuItems = [
+export const tempFinanceMenuContent: SidebarAppSubMenuItems = [
 	{ id: 1, label: 'Barclays' },
 	{ id: 2, label: 'Lloyds' },
 	{ id: 3, label: 'Very Store Account' },
