512ed72efe0b5fa4ba81a21f1b88aaeab1369e79
ionztorm <leon.codes@icloud.com>
2024-11-28 19:24:09 +0000

    basic structure for user registration server action with useActionState state

--- src/actions/auth.actions.ts
+++ src/actions/auth.actions.ts
@@ -5,20 +5,20 @@ import { SignupSchema } from '@/lib/validations/schema/auth.email.signup.schema'
 
 export type SignupState = {
 	error: ZodError<{ name: string; email: string; password: string }> | null;
-	data: string | null;
+	status: 'success' | 'error';
 };
 
 export const registerUser = async (_: SignupState, formData: FormData): Promise<SignupState> => {
 	const parsedFormData = SignupSchema.safeParse(formData);
 	if (!parsedFormData.success) {
 		return {
-			data: null,
+			status: 'error',
 			error: parsedFormData.error,
 		};
 	}
 
 	return {
-		data: 'Registration Success!',
+		status: 'success',
 		error: null,
 	};
 };
