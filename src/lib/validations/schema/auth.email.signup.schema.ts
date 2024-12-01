277f0aead6ca82b3f1b611ff41d04b1808860f64
ionztorm <leon.codes@icloud.com>
2024-12-01 19:31:08 +0000

    update schema

--- src/lib/validations/schema/auth.email.signup.schema.ts
+++ src/lib/validations/schema/auth.email.signup.schema.ts
@@ -9,7 +9,7 @@ export const SignupSchema = z
 		email: z
 			.string()
 			.email({ message: 'Invalid email address' })
-			.min(1, { message: 'Email is required' }),
+			.min(5, { message: 'Email is required' }),
 		password: z
 			.string()
 			.min(8, { message: 'Password must be at least 6 characters long' })
