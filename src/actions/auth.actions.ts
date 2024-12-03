8e428cc4b73a71e8a00bea7067c22fe182c684bd
ionztorm <leon.codes@icloud.com>
2024-12-03 18:35:19 +0000

    update login form to RHF, create login SA and make form functional

--- src/actions/auth.actions.ts
+++ src/actions/auth.actions.ts
@@ -1,27 +1,19 @@
 'use server';
 
 import { auth } from '@/lib/auth';
-import type { Signup } from '@/lib/types/validation.types';
+import type { LoginFormErrors, SignupFormErrors } from '@/lib/types/auth/auth.types';
+import type { Login, Signup } from '@/lib/types/validation.types';
+import { LoginSchema } from '@/lib/validations/schema/auth.email.login.schema';
 import { SignupSchema } from '@/lib/validations/schema/auth.email.signup.schema';
 import { APIError } from 'better-auth/api';
 
-export type UserRegistrationState =
-	| { errors: FormErrors; name: null }
-	| { errors: null; name: string };
-export type FormErrors = Readonly<{
-	name?: string[];
-	email?: string[];
-	password?: string[];
-	confirmPassword?: string[];
-	signup?: string[];
-}>;
+export type UserRegistrationState = { errors: SignupFormErrors | null };
 
 export const registerUserAction = async (values: Signup): Promise<UserRegistrationState> => {
 	const parsedFormData = SignupSchema.safeParse(values);
 	if (!parsedFormData.success) {
 		return {
 			errors: parsedFormData.error.flatten().fieldErrors,
-			name: null,
 		};
 	}
 
@@ -41,28 +33,55 @@ export const registerUserAction = async (values: Signup): Promise<UserRegistrati
 
 		if (!data?.user?.name) {
 			return {
-				errors: { signup: ['Unexpected response: user name is missing'] },
-				name: null,
+				errors: { saving: ['Unexpected response: user name is missing'] },
 			};
 		}
 
 		return {
 			errors: null,
-			name: data.user.name,
 		};
 	} catch (error: unknown) {
 		// Catch API errors and return a matching error message
 		if (error instanceof APIError) {
 			return {
-				errors: { signup: [error.message] },
-				name: null,
+				errors: { saving: [error.message] },
 			};
 		}
 
 		// Handle unexpected errors in a consistent way
 		return {
-			errors: { signup: ['An unexpected error occurred'] },
-			name: null,
+			errors: { saving: ['An unexpected error occurred'] },
+		};
+	}
+};
+
+export type LoginFormState = { errors: LoginFormErrors | null };
+
+export const loginUserAction = async (values: Login): Promise<LoginFormState> => {
+	const parsedFormData = LoginSchema.safeParse(values);
+
+	if (!parsedFormData.success) {
+		return {
+			errors: parsedFormData.error.flatten().fieldErrors,
+		};
+	}
+
+	try {
+		await auth.api.signInEmail({
+			body: {
+				...parsedFormData.data,
+			},
+		});
+		return {
+			errors: null,
+		};
+	} catch (error: unknown) {
+		if (error instanceof APIError)
+			return {
+				errors: { saving: ['There was an issue logging in'] },
+			};
+		return {
+			errors: { saving: ['An unexpected error occurred'] },
 		};
 	}
 };
