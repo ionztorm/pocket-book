d5ef4b98daec90e999851c6b156c7794ae43d199
ionztorm <leon.codes@icloud.com>
2024-12-05 19:26:08 +0000

    resend email setup for otp

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,8 @@ import { env } from '@/lib/validations/validators/env.server.validator';
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { nextCookies } from 'better-auth/next-js';
+import { emailOTP } from 'better-auth/plugins';
+import { resend } from './resend';
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
@@ -28,5 +30,26 @@ export const auth = betterAuth({
 			clientSecret: env.AUTH_GITHUB_SECRET,
 		},
 	},
-	plugins: [nextCookies()],
+	plugins: [
+		nextCookies(),
+		emailOTP({
+			async sendVerificationOTP({ email, otp, type }) {
+				if (type === 'sign-in') {
+					await resend.emails.send({
+						from: 'Test <onboarding@resend.dev>',
+						to: email,
+						subject: 'Your One-Time-Password for Pocket Book',
+						html: `Your one time password is valid for 5  minutes: ${otp}`,
+					});
+				} else {
+					await resend.emails.send({
+						from: 'Test <onboarding@resend.dev>',
+						to: email,
+						subject: 'Pocket Book - Verify your email address',
+						html: `Thanks for choosing Pocket Book. You need to verify your email. Please proceed to login using this one-time-password: ${otp}. Your password is valid for 5 minutes.`,
+					});
+				}
+			},
+		}),
+	],
 });
