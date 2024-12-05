ea344bbd20d5ad8bc912fad5800439b926b69565
ionztorm <leon.codes@icloud.com>
2024-12-05 18:41:45 +0000

    update env, env schema, example for resend

--- src/lib/validations/schema/env.var.schema.ts
+++ src/lib/validations/schema/env.var.schema.ts
@@ -3,22 +3,25 @@ import { z } from 'zod';
 export const EnvironmentSchema = z.object({
 	DATABASE_URL: z.string(),
 	BETTER_AUTH_SECRET: z.string(),
-	BETTER_AUTH_URL: z.string().url(),
+	NEXT_PUBLIC_APP_URL: z.string().url(),
 	AUTH_GOOGLE_ID: z.string(),
 	AUTH_GOOGLE_SECRET: z.string(),
 	AUTH_GITHUB_ID: z.string(),
 	AUTH_GITHUB_SECRET: z.string(),
+	RESEND_KEY: z.string(),
+	ENV: z.enum(['development', 'testing', 'production']),
 });
 
 declare global {
 	interface ProcessEnv {
 		DATABASE_URL: string;
 		BETTER_AUTH_SECRET: string;
-		BETTER_AUTH_URL: string;
+		NEXT_PUBLIC_APP_URL: string;
 		AUTH_GOOGLE_ID: string;
 		AUTH_GOOGLE_SECRET: string;
 		AUTH_GITHUB_ID: string;
 		AUTH_GITHUB_SECRET: string;
+		RESEND_KEY: string;
 		ENV: 'development' | 'testing' | 'production';
 	}
 }
