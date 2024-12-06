5cbb66e1bbb1bd803a619dfbdfbfb07b4f1e7e19
ionztorm <leon.codes@icloud.com>
2024-12-06 20:27:05 +0000

    add some wrappers to auth layout

--- src/app/(auth)/auth/layout.tsx
+++ src/app/(auth)/auth/layout.tsx
@@ -1,6 +1,13 @@
 import type { ChildrenProps } from '@/lib/types/global.types';
 import { AuthenticationContextProvider } from '../_context/auth-context';
+import { Card } from '@/components/ui/card';
 
 export default function AuthLayout({ children }: ChildrenProps) {
-	return <AuthenticationContextProvider>{children}</AuthenticationContextProvider>;
+	return (
+		<AuthenticationContextProvider>
+			<div className='grid min-h-[100dvh] w-full place-items-center'>
+				<Card className='mx-auto w-full max-w-sm'>{children}</Card>
+			</div>
+		</AuthenticationContextProvider>
+	);
 }
