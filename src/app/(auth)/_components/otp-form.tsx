045e57aa2268d341a9ba2ea65ef994033c2a0320
ionztorm <leon.codes@icloud.com>
2024-12-07 18:01:13 +0000

    upate otp form as a reusable form

--- src/app/(auth)/_components/otp-form.tsx
+++ src/app/(auth)/_components/otp-form.tsx
@@ -1,36 +1,71 @@
 import { Button } from '@/components/ui/button';
 import {
-	Dialog,
 	DialogClose,
-	DialogContent,
 	DialogDescription,
 	DialogFooter,
 	DialogHeader,
 	DialogTitle,
 } from '@/components/ui/dialog';
+import { Form, FormControl, FormField, FormItem, FormLabel } from '@/components/ui/form';
+import {
+	InputOTP,
+	InputOTPGroup,
+	InputOTPSeparator,
+	InputOTPSlot,
+} from '@/components/ui/input-otp';
+import type { OTP } from '@/lib/types/validation.types';
+import { OTPSchema } from '@/lib/validations/schema/auth.email.login.schema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 
-export type OTPFormProps = Readonly<{
-	isOpen: boolean;
-	setIsOpen: (isOpen: boolean) => void;
-}>;
-
-export function OTPForm({ isOpen, setIsOpen }: OTPFormProps) {
+export function OTPForm() {
+	const form = useForm<OTP>({
+		resolver: zodResolver(OTPSchema),
+		defaultValues: {
+			otp: '',
+		},
+	});
 	return (
-		<Dialog open={isOpen} onOpenChange={setIsOpen}>
-			<DialogContent>
-				<DialogHeader>
-					<DialogTitle>OTP Bro</DialogTitle>
-					<DialogDescription>
-						You should have received an email with a 6 digit code. Please enter it below. The is
-						valid for 5 minutes.
-					</DialogDescription>
-				</DialogHeader>
-				<DialogFooter>
-					<DialogClose asChild>
-						<Button type='submit'>I&apos;ve got it, let me in!</Button>
-					</DialogClose>
-				</DialogFooter>
-			</DialogContent>
-		</Dialog>
+		<>
+			<DialogHeader className='items-center'>
+				<DialogTitle>OTP Bro</DialogTitle>
+				<DialogDescription asChild>
+					<p>Please enter your code. The code is valid for 5 minutes.</p>
+				</DialogDescription>
+			</DialogHeader>
+			<Form {...form}>
+				<form>
+					<FormField
+						control={form.control}
+						name='otp'
+						render={({ field }) => (
+							<FormItem className='flex flex-col items-center gap-2'>
+								<FormLabel className='sr-only'>Enter your one time password</FormLabel>
+								<FormControl>
+									<InputOTP maxLength={6} {...field}>
+										<InputOTPGroup>
+											<InputOTPSlot index={0} />
+											<InputOTPSlot index={1} />
+											<InputOTPSlot index={2} />
+										</InputOTPGroup>
+										<InputOTPSeparator />
+										<InputOTPGroup>
+											<InputOTPSlot index={3} />
+											<InputOTPSlot index={4} />
+											<InputOTPSlot index={5} />
+										</InputOTPGroup>
+									</InputOTP>
+								</FormControl>
+							</FormItem>
+						)}
+					/>
+				</form>
+			</Form>
+			<DialogFooter>
+				<DialogClose asChild>
+					<Button type='submit'>Submit</Button>
+				</DialogClose>
+			</DialogFooter>
+		</>
 	);
 }
