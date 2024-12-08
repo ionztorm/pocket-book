d676d88560223368014c0752ec5ad392406bae1e
ionztorm <leon.codes@icloud.com>
2024-12-08 22:15:11 +0000

    remove name from registration form

--- src/app/(auth)/_components/register-form.tsx
+++ src/app/(auth)/_components/register-form.tsx
@@ -12,8 +12,7 @@ import {
 import { Input } from '@/components/ui/input';
 import { Loading } from '@/components/ui/loading';
 import type { SignupFormErrors } from '@/lib/types/auth/auth.types';
-import type { Signup } from '@/lib/types/validation.types';
-import { SignupSchema } from '@/lib/validations/schema/auth.email.signup.schema';
+import type { Email } from '@/lib/types/validation.types';
 import { zodResolver } from '@hookform/resolvers/zod';
 import Link from 'next/link';
 import { useState } from 'react';
@@ -21,6 +20,7 @@ import { useForm } from 'react-hook-form';
 
 import { Separator } from '@/components/ui/separator';
 import { sendOtpEmail } from '@/lib/utils/otpUtils';
+import { EmailSchema } from '@/lib/validations/schema/auth.email.login.schema';
 import { toast } from 'sonner';
 import { useAuthenticationContext } from '../_context/auth-context';
 import { OTPForm } from './otp-form';
@@ -29,25 +29,16 @@ export function RegisterForm() {
 	const [errors, _setErrors] = useState<SignupFormErrors | null>(null);
 	const [isOpen, setIsOpen] = useState(false);
 	const { dispatch: registerDispatch } = useAuthenticationContext();
-	const form = useForm<Signup>({
-		resolver: zodResolver(SignupSchema),
+	const form = useForm<Email>({
+		resolver: zodResolver(EmailSchema),
 		defaultValues: {
-			name: '',
 			email: '',
 		},
 	});
 	const isPending = form.formState.isSubmitting;
 
-	const onSubmit = async (values: Signup) => {
-		// const data = await authClient.emailOtp.sendVerificationOtp({
-		// 	email: values.email,
-		// 	type: 'email-verification',
-		// });
-		const data = await sendOtpEmail(values.email, 'email-verification');
-		console.log(data);
-		// const data = await signInWithOtp(values.email, values.otp);
-		console.log(values);
-		registerDispatch({ type: 'name', name: values.name });
+	const onSubmit = async (values: Email) => {
+		await sendOtpEmail(values.email, 'email-verification');
 		registerDispatch({ type: 'email', email: values.email });
 		toast.success("We've sent you a one time password. Please check your emails.");
 		setIsOpen(true);
@@ -62,24 +53,6 @@ export function RegisterForm() {
 			<CardContent className='grid gap-4'>
 				<Form {...form}>
 					<form onSubmit={form.handleSubmit(onSubmit)} className='grid gap-3'>
-						<FormField
-							control={form.control}
-							name='name'
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel className='sr-only'>Name</FormLabel>
-									<FormControl>
-										<Input
-											disabled={isPending}
-											autoComplete='name'
-											placeholder='Joe Bloggs'
-											{...field}
-										/>
-									</FormControl>
-									<FormMessage>{errors?.name ? errors.name : ''}</FormMessage>
-								</FormItem>
-							)}
-						/>
 						<FormField
 							control={form.control}
 							name='email'
@@ -104,7 +77,7 @@ export function RegisterForm() {
 						</Button>
 					</form>
 				</Form>
-				<OTPForm type='email-verification' isOpen={isOpen} setIsOpen={setIsOpen} />
+				<OTPForm isOpen={isOpen} setIsOpen={setIsOpen} />
 				<Separator />
 				<SocialLogins />
 				<Separator />
