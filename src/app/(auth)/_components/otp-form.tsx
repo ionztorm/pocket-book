d67e7af4b22c987be20faf6d973aae31e207b049
ionztorm <leon.codes@icloud.com>
2024-12-08 22:10:45 +0000

    combine otp dialog and otp form components

--- src/app/(auth)/_components/otp-form.tsx
+++ src/app/(auth)/_components/otp-form.tsx
@@ -1,6 +1,8 @@
 import { Button } from '@/components/ui/button';
 import {
+	Dialog,
 	DialogClose,
+	DialogContent,
 	DialogDescription,
 	DialogFooter,
 	DialogHeader,
@@ -13,14 +15,19 @@ import {
 	InputOTPSeparator,
 	InputOTPSlot,
 } from '@/components/ui/input-otp';
+import type { OTPFormProps } from '@/lib/types/auth/auth.types';
 import type { OTP } from '@/lib/types/validation.types';
+import { signInWithOtp } from '@/lib/utils/otpUtils';
 import { OTPSchema } from '@/lib/validations/schema/auth.email.login.schema';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
 import { useAuthenticationContext } from '../_context/auth-context';
 
-export function OTPForm() {
-	const { dispatch: otpDispatch } = useAuthenticationContext();
+export function OTPForm({ isOpen, setIsOpen }: OTPFormProps) {
+	const { dispatch: otpDispatch, state } = useAuthenticationContext();
+	const router = useRouter();
 	const form = useForm<OTP>({
 		resolver: zodResolver(OTPSchema),
 		defaultValues: {
@@ -28,54 +35,73 @@ export function OTPForm() {
 		},
 	});
 
-	const onSubmit = (values: OTP) => {
+	const onSubmit = async (values: OTP) => {
 		console.log(values);
-		otpDispatch({ type: 'otp', otp: values.otp });
+		// check if email exists
+		if (!state.email) {
+			toast.error("It looks as though we don't have your email address");
+			router.push('/auth/register');
+			return null;
+		}
+
+		// attempt to sign in with OTP
+		const data = await signInWithOtp(state.email, values.otp);
+		console.log(data);
+		if (data.error) return toast.error(data.error.message);
+		// reset state and redirect
+		otpDispatch({ type: 'otp', otp: '' });
+		otpDispatch({ type: 'email', email: '' });
+		otpDispatch({ type: 'name', name: '' });
+		toast.success('You have successfully logged in');
+		return router.push('/dashboard/todo');
 	};
+
 	return (
-		<>
-			<DialogHeader className='items-center'>
-				<DialogTitle>Enter your One Time Password</DialogTitle>
-				<DialogDescription asChild>
-					<p>
-						Enter the temporary 6 digit code we sent to your email address. It's only valid for 5
-						minutes. You can copy and paste the code from the email if you like.
-					</p>
-				</DialogDescription>
-			</DialogHeader>
-			<Form {...form}>
-				<form onSubmit={form.handleSubmit(onSubmit)}>
-					<FormField
-						control={form.control}
-						name='otp'
-						render={({ field }) => (
-							<FormItem className='flex flex-col items-center gap-2'>
-								<FormLabel className='sr-only'>Enter your one time password</FormLabel>
-								<FormControl>
-									<InputOTP maxLength={6} {...field}>
-										<InputOTPGroup>
-											<InputOTPSlot index={0} />
-											<InputOTPSlot index={1} />
-											<InputOTPSlot index={2} />
-										</InputOTPGroup>
-										<InputOTPSeparator />
-										<InputOTPGroup>
-											<InputOTPSlot index={3} />
-											<InputOTPSlot index={4} />
-											<InputOTPSlot index={5} />
-										</InputOTPGroup>
-									</InputOTP>
-								</FormControl>
-							</FormItem>
-						)}
-					/>
-					<DialogFooter>
-						<DialogClose asChild>
-							<Button type='submit'>Submit</Button>
-						</DialogClose>
-					</DialogFooter>
-				</form>
-			</Form>
-		</>
+		<Dialog open={isOpen} onOpenChange={setIsOpen}>
+			<DialogContent className='grid gap-4'>
+				<DialogHeader className='items-center gap-2'>
+					<DialogTitle>Enter your One Time Password</DialogTitle>
+					<DialogDescription asChild>
+						<p>
+							We've sent a temporary code to {state.email}. It's only valid for 5 minutes. Go get it
+							and paste it here.
+						</p>
+					</DialogDescription>
+				</DialogHeader>
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(onSubmit)} className='grid gap-4'>
+						<FormField
+							control={form.control}
+							name='otp'
+							render={({ field }) => (
+								<FormItem className='flex flex-col items-center gap-2'>
+									<FormLabel className='sr-only'>Enter your one time password</FormLabel>
+									<FormControl>
+										<InputOTP maxLength={6} {...field}>
+											<InputOTPGroup>
+												<InputOTPSlot index={0} />
+												<InputOTPSlot index={1} />
+												<InputOTPSlot index={2} />
+											</InputOTPGroup>
+											<InputOTPSeparator />
+											<InputOTPGroup>
+												<InputOTPSlot index={3} />
+												<InputOTPSlot index={4} />
+												<InputOTPSlot index={5} />
+											</InputOTPGroup>
+										</InputOTP>
+									</FormControl>
+								</FormItem>
+							)}
+						/>
+						<DialogFooter>
+							<DialogClose asChild>
+								<Button type='submit'>Submit</Button>
+							</DialogClose>
+						</DialogFooter>
+					</form>
+				</Form>
+			</DialogContent>
+		</Dialog>
 	);
 }
