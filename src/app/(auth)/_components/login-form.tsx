00b6949279fbd4a462eb74388bdbd5baab00c7db
ionztorm <leon.codes@icloud.com>
2024-12-23 15:45:20 +0000

    cleanup unused code, backup unused components

--- src/app/(auth)/_components/login-form.tsx
+++ src/app/(auth)/_components/login-form.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useAuthenticationContext } from '@/app/(auth)/_context/auth-context';
 import { Button } from '@/components/ui/button';
 import {
 	Form,
@@ -18,12 +19,10 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import Link from 'next/link';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { useAuthenticationContext } from '../_context/auth-context';
-import { OTPForm } from './otp-form';
 
 export function LoginForm() {
 	const [errors, _setErrors] = useState<LoginFormErrors | null>(null);
-	const [isOpen, setIsOpen] = useState(false);
+	const [_isOpen, _setIsOpen] = useState(false);
 	const { setEmail } = useAuthenticationContext();
 	const form = useForm<Login>({
 		resolver: zodResolver(LoginSchema),
@@ -56,61 +55,58 @@ export function LoginForm() {
 	};
 
 	return (
-		<>
-			<Form {...form}>
-				<form onSubmit={form.handleSubmit(onSubmit)} className='grid gap-6'>
-					<FormField
-						control={form.control}
-						name='email'
-						render={({ field }) => (
-							<FormItem className='grid gap-2'>
-								<FormLabel>Email</FormLabel>
-								<FormControl>
-									<Input
-										disabled={form.formState.isSubmitting}
-										placeholder='joe.bloggs@example.com'
-										autoComplete='email'
-										type='email'
-										className='rounded-lg shadow-sm'
-										{...field}
-									/>
-								</FormControl>
-								<FormMessage>{errors?.email ? errors.email : ''}</FormMessage>
-							</FormItem>
-						)}
-					/>
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className='grid gap-6'>
+				<FormField
+					control={form.control}
+					name='email'
+					render={({ field }) => (
+						<FormItem className='grid gap-2'>
+							<FormLabel>Email</FormLabel>
+							<FormControl>
+								<Input
+									disabled={form.formState.isSubmitting}
+									placeholder='joe.bloggs@example.com'
+									autoComplete='email'
+									type='email'
+									className='rounded-lg shadow-sm'
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage>{errors?.email ? errors.email : ''}</FormMessage>
+						</FormItem>
+					)}
+				/>
 
-					<FormField
-						control={form.control}
-						name='password'
-						render={({ field }) => (
-							<FormItem className='grid gap-2'>
-								<div className='flex w-full items-center justify-between'>
-									<FormLabel>Password</FormLabel>
-									<Link href='#' className='text-muted-foreground text-sm hover:underline'>
-										Forgot password?
-									</Link>
-								</div>
-								<FormControl>
-									<Input
-										type='password'
-										disabled={form.formState.isSubmitting}
-										placeholder='*******'
-										autoComplete='current-password'
-										className='rounded-lg shadow-sm'
-										{...field}
-									/>
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<Button type='submit' className='w-full flex-1 rounded-lg shadow-sm'>
-						{isPending ? <Loading /> : 'Login'}
-					</Button>
-				</form>
-			</Form>
-			<OTPForm isOpen={isOpen} setIsOpen={setIsOpen} otpFormType='sign-in' />
-		</>
+				<FormField
+					control={form.control}
+					name='password'
+					render={({ field }) => (
+						<FormItem className='grid gap-2'>
+							<div className='flex w-full items-center justify-between'>
+								<FormLabel>Password</FormLabel>
+								<Link href='#' className='text-muted-foreground text-sm hover:underline'>
+									Forgot password?
+								</Link>
+							</div>
+							<FormControl>
+								<Input
+									type='password'
+									disabled={form.formState.isSubmitting}
+									placeholder='*******'
+									autoComplete='current-password'
+									className='rounded-lg shadow-sm'
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<Button type='submit' className='w-full flex-1 rounded-lg shadow-sm'>
+					{isPending ? <Loading /> : 'Login'}
+				</Button>
+			</form>
+		</Form>
 	);
 }
--- src/app/(auth)/_components/sign-up-form.tsx
+++ src/app/(auth)/_components/sign-up-form.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useAuthenticationContext } from '@/app/(auth)/_context/auth-context';
 import { Button } from '@/components/ui/button';
 import {
 	Form,
@@ -10,19 +11,15 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Loading } from '@/components/ui/loading';
-import type { Signup } from '@/lib/types/validation.types';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
-
-import { OTPForm } from '@/app/(auth)/_components/otp-form';
-import { useAuthenticationContext } from '@/app/(auth)/_context/auth-context';
 import { authClient } from '@/lib/auth-client';
+import type { Signup } from '@/lib/types/validation.types';
 import { SignupSchema } from '@/lib/validations/schema/auth.email.auth.schema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 import { toast } from 'sonner';
 
 export function SignUpForm() {
-	const [isOpen, setIsOpen] = useState(false);
 	const { setEmail } = useAuthenticationContext();
 
 	const form = useForm<Signup>({
@@ -36,25 +33,20 @@ export function SignUpForm() {
 	const isPending = form.formState.isSubmitting;
 
 	const onSubmit = async (values: Signup) => {
-		try {
-			await authClient.signUp.email(
-				{
-					...values,
+		await authClient.signUp.email(
+			{
+				...values,
+			},
+			{
+				onSuccess: () => {
+					toast.success('Welcome! Please check your emails and verify your email address.');
+					setEmail(values.email);
 				},
-				{
-					onSuccess: () => {
-						toast.success('Welcome! Please check your emails and verify your email address.');
-						setEmail(values.email);
-						setIsOpen(true);
-					},
-					onError: (ctx) => {
-						toast.error(ctx.error.message);
-					},
+				onError: (ctx) => {
+					toast.error(ctx.error.message);
 				},
-			);
-		} catch (err) {
-			console.log(err);
-		}
+			},
+		);
 	};
 
 	const emailValue = form.watch('email');
@@ -64,68 +56,65 @@ export function SignUpForm() {
 	}, [form.setValue, emailValue]);
 
 	return (
-		<>
-			<Form {...form}>
-				<form onSubmit={form.handleSubmit(onSubmit)} className='grid gap-6'>
-					<FormField
-						control={form.control}
-						name='name'
-						render={({ field }) => (
-							<FormItem className='hidden'>
-								<FormControl>
-									<Input type='hidden' {...field} />
-								</FormControl>
-							</FormItem>
-						)}
-					/>
-					<FormField
-						control={form.control}
-						name='email'
-						render={({ field }) => (
-							<FormItem className='grid gap-2'>
-								<FormLabel>Email</FormLabel>
-								<FormControl>
-									<Input
-										disabled={isPending}
-										autoComplete='email'
-										placeholder='joe.bloggs@example.com'
-										className='rounded-lg shadow-sm'
-										type='email'
-										{...field}
-									/>
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className='grid gap-6'>
+				<FormField
+					control={form.control}
+					name='name'
+					render={({ field }) => (
+						<FormItem className='hidden'>
+							<FormControl>
+								<Input type='hidden' {...field} />
+							</FormControl>
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name='email'
+					render={({ field }) => (
+						<FormItem className='grid gap-2'>
+							<FormLabel>Email</FormLabel>
+							<FormControl>
+								<Input
+									disabled={isPending}
+									autoComplete='email'
+									placeholder='joe.bloggs@example.com'
+									className='rounded-lg shadow-sm'
+									type='email'
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
 
-					<FormField
-						control={form.control}
-						name='password'
-						render={({ field }) => (
-							<FormItem className='grid gap-2'>
-								<FormLabel>Password</FormLabel>
-								<FormControl>
-									<Input
-										disabled={isPending}
-										autoComplete='new-password'
-										placeholder='*******'
-										type='password'
-										className='rounded-lg shadow-sm'
-										{...field}
-									/>
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
+				<FormField
+					control={form.control}
+					name='password'
+					render={({ field }) => (
+						<FormItem className='grid gap-2'>
+							<FormLabel>Password</FormLabel>
+							<FormControl>
+								<Input
+									disabled={isPending}
+									autoComplete='new-password'
+									placeholder='*******'
+									type='password'
+									className='rounded-lg shadow-sm'
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
 
-					<Button type='submit' className='rounded-lg shadow-sm' disabled={isPending}>
-						{isPending ? <Loading /> : 'Register'}
-					</Button>
-				</form>
-			</Form>
-			<OTPForm isOpen={isOpen} setIsOpen={setIsOpen} />
-		</>
+				<Button type='submit' className='rounded-lg shadow-sm' disabled={isPending}>
+					{isPending ? <Loading /> : 'Register'}
+				</Button>
+			</form>
+		</Form>
 	);
 }
